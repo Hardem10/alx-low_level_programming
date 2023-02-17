c3644cc1a22222c21040d3249cd3bc8a17295d81
Hardem10 <ademorotitope@gmail.com>
2023-02-17 03:57:11 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - A c program that print a line to the standard error
  * Return: 1 (Success)
- */
-
+ */i
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+
 }
