e68b374694a7cf6b210aac59cbd5b11401bfb2a7
Hardem10 <ademorotitope@gmail.com>
2023-02-23 17:55:19 -0800

    debuggig

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#ifndef MAIN_H
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_numbe(int a, int b, int c);
+void print_remaining days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif
